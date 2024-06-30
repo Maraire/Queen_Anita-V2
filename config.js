ad043dcbbb1145b24421f68d9714add9e54dd3c0
Maraire <tanatswamaraire06@gmail.com>
2024-06-30 16:58:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
 
 //=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUdhS2UyWDdoMUlDUUltdkVLS21Iemx4SnQ3czl5SnVFRGEyT3ZIQTZFQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV1ptVGlMQnRWdlRDeHhBczJXOWZOMUozU3h4a2NjWVVIaEdIUkxlMjQzMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJpRXVFWnFXVU1Oc05qNWxIVWdVRndDWlExYW4xS1JvKzJrcVg4b01KVFU0PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJSOVg4Sk1oSEF0QnloT1dLUXNSL0JmYTBneHpDOWJ6bVd3R2dFcG91K0dZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImNLTUozZXkrSi9TSzhFUWtRazNrQXk3ZDRBSTB0dmRpbkRhZTB2YWh6Vlk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InZJa05Ickl1TVlMZml1RDQzQUs2OXNZcjBEYllJVVU5aHBFdDN6OG01eTA9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid0U4Zjk2Y3hWNDEzQ3lZMXdHWmpkRk1PNlpkUnI4aFMyZzkwclNhdGQyZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiejAyelBJdXB6NE5Ua2JPYkhoNGVTL3VDRkkwWGw2Umc0YXVZbzVOY1lCYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkpFWXFSTWZHVmMwaXVEOTFEbU9FbVQrdFVaSDNxbXd2Rks3dE1qNHliMUVTRm9qZHFudE1OaXQrYU1jVktzb0VVOG1ZV1lldEs0TXo4dzF2WEFjaUJnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6OTAsImFkdlNlY3JldEtleSI6IlRJOWhBQWtxUDg4Z0pqb0QvRmovVjlqSlpoUFlJN3FJWW85OFFmVmJuU2M9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IjZXTFVCc0pqVEs2M3BPX1p6TWtBSUEiLCJwaG9uZUlkIjoiZGZiZGRlYzUtNGE2NC00MTZkLTk5MmItNzk0YzdhZDI3NmExIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlVxVG9mbCtkWSt2UTlTbHZrNXV5SFdZNXcvbz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJSdDhLQ0JZVm95bUlPN211VnFzbU1yMjc0ZFk9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiRUc3OTZaRFkiLCJtZSI6eyJpZCI6IjI2MzcxNDg0NDM1Mzo0QHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNQM2xpVVVRb2VLRnRBWVlCQ0FBS0FBPSIsImFjY291bnRTaWduYXR1cmVLZXkiOiIvd0R6SmJEalp6NDZ1UkN3S3NHRlRWYmNTcGRiWDBHOFY3SW1SUlhpc3c0PSIsImFjY291bnRTaWduYXR1cmUiOiJ2a3J0MUtNVDVLbzhNRDlQZCtVd2ZzcEE0NWpCQVk4MmtjYkpseER3UXN3VTc3RThJMWcveDFRM0pxMldJZGFsNzJSRjZEWTdWbkVROTlHTHAySVBBQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiRWc2bC8wSFB0b0pkSVI4MXdoek41TTNLdlVUckl6VEw4bXA5dXplOWtwN1RUQlArT0Nsa1gvVERvL1VlcVdoczlxc0NUUFpiZ2NKZEdlRGs0dUVNQnc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyNjM3MTQ4NDQzNTM6NEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJmOEE4eVd3NDJjK09ya1FzQ3JCaFUxVzNFcVhXMTlCdkZleUprVVY0ck1PIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzE5NzU5MTUwfQ==";
 global.MONGODB = process.env.MONGODB_URI || "";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.sudo = process.env.SUDO
@@ -12,7 +12,7 @@ global.sudo = process.env.SUDO
   : "null";
 global.owner = process.env.OWNER_NUMBER
   ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
-  : "2349066528353";
+  : "263786997108";
 global.THUMB_IMAGE =
   process.env.THUMB_IMAGE ||
   process.env.IMAGE ||
@@ -30,8 +30,8 @@ module.exports = {
   caption: process.env.CAPTION || "`©QUEEN_ANITA-V2`",
   author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
   packname: process.env.PACK_NAME || "A N I T A",
-  botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  botname: process.env.BOT_NAME || "Mr Maraire Official",
+  ownername: process.env.OWNER_NAME || "Mr Maraire",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
